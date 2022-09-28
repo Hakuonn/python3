f943f2aefa3d787a3291099fa5aa31257ee405e8
趙伯恩 <hakuonn@hakuonn-MacBook.local>
2022-09-28 21:47:20 +0800

    first commit

--- 0928_GIL_Canvas.py/ch7_1.py
+++ 0928_GIL_Canvas.py/ch7_1.py
@@ -1,11 +1,34 @@
 import tkinter as tk
-from PIL import Image, ImageTk
-# img
+
+
 root = tk.Tk()
 root.title('C110156217')
-img = Image.open('ok.png')
-img2 = ImageTk.PhotoImage(img)
-mycanvas = tk.Canvas(root, width=img.size[0], height=img.size[1])
+mycanvas = tk.Canvas(root, width=500, height=500)
 mycanvas.pack()
-mycanvas.create_image(0, 0, anchor=tk.NW, image=img2)
+
+#  橢圓
+mycanvas.create_oval(60, 290, 230, 410)
+
+# 直線
+mycanvas.create_line(60, 260, 85, 300)
+
+# text
+mycanvas.create_text(145, 350, text="我全都要", font=('微軟正黑體', 30))
+
+# 三角
+mycanvas.create_polygon(25, 100, 110, 100, 65, 55, fill='red')
+
+# 矩形
+mycanvas.create_rectangle(5, 130, 110, 250, fill='blue')
+mycanvas.create_rectangle(35, 100, 95, 170, fill='white')
+mycanvas.create_rectangle(45, 100, 90, 150, fill='#DEB887')
+
+mycanvas.create_rectangle(150, 140, 240, 250, fill='gray')
+mycanvas.create_rectangle(160, 100, 210, 140, fill='#DEB887')
+mycanvas.create_rectangle(160, 60, 210, 100, fill='black')
+
+# 多邊形
+mycanvas.create_polygon(360, 360, 380, 310, 400, 360,
+                        400, 400, 360, 400, fill='black')
+
 root.mainloop()
---
+++ 0928_GIL_Canvas.py/ch7_1_image.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+# img
+root = tk.Tk()
+root.title('C110156217')
+img = Image.open('meme.png')
+img2 = ImageTk.PhotoImage(img)
+mycanvas = tk.Canvas(root, width=img.size[0], height=img.size[1])
+mycanvas.pack()
+mycanvas.create_image(0, 0, anchor=tk.NW, image=img2)
+root.mainloop()
