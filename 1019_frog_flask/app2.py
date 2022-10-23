cb694a0b26402e4875cd5ecdef47a0563053b694
趙伯恩 <hakuonn@hakuonn-MacBook.local>
2022-10-23 17:10:01 +0800

    first commit

--- 1019_frog_flask/app2.py
+++ 1019_frog_flask/app2.py
@@ -3,8 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/") #根目錄
 @app.route("/idandname")
-@app.route("/test")
 def hello():
     return "C110156217 趙伯恩"
+
+@app.route("/test")
+def test():
+    return "<h1>ok打ok  ok<h1>"
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port='8080')
